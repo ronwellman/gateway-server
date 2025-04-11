fff43fccb00a9c6d18b7426131d53eefaed5856e
MattHays2 <mhays@distorted.dev>
2025-04-10 22:01:54 -0600

    Logging

--- src/main/java/com/courseproject/gateway_server/filters/ResponseFilter.java
+++ src/main/java/com/courseproject/gateway_server/filters/ResponseFilter.java
@@ -1,18 +1,23 @@
 package com.courseproject.gateway_server.filters;
 
+import io.micrometer.tracing.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 public class ResponseFilter {
  
     final Logger logger =LoggerFactory.getLogger(ResponseFilter.class);
+
+    @Autowired
+    private Tracer tracer;
     
     @Autowired
 	FilterUtils filterUtils;
@@ -20,11 +25,11 @@ public class ResponseFilter {
     @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
+            String traceId = tracer.currentSpan() != null ? Objects.requireNonNull(tracer.currentSpan()).context().traceId() :
+                    exchange.getRequest().getHeaders().getFirst(FilterUtils.CORRELATION_ID);
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            	  HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-            	  String correlationId = filterUtils.getCorrelationId(requestHeaders);
-            	  logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
-                  exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+            	  logger.debug("Adding the correlation id to the outbound headers. {}", traceId);
+                  exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
                   logger.debug("Completing outgoing request for {}.", exchange.getRequest().getURI());
               }));
         };
